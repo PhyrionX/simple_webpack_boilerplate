c20f2a0872c24bc29f3ca540f895a4940767c1bc
Ruben Gabas <ruben.gabas@adidas.com>
2019-09-20 15:23:42 +0200

    config scss and fixes

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
@@ -18,6 +18,9 @@ module.exports = {
       },
       ...scssRules
     ]
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx' ]
   },
   plugins: [htmlPlugin]
 };
